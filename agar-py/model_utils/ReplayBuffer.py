3762c70b915961f0c7f0584f05eacaf5c8a6f4ad
cacabo <ccabo@seas.upenn.edu>
2020-04-26 20:27:47 -0400

    update todos

--- agar-py/model_utils/ReplayBuffer.py
+++ agar-py/model_utils/ReplayBuffer.py
@@ -1,20 +1,21 @@
 import random
 
+
 class ReplayBuffer():
     def __init__(self, capacity):
         self.capacity = capacity
         self.buffer = []
         self.idx = 0
-    
-    # save a state transition memory
+
     def push(self, memory):
+        """save a state transition memory"""
         if len(self.buffer) < self.capacity:
             self.buffer.append(None)
         self.buffer[self.idx] = memory
         self.idx = (self.idx + 1) % self.capacity
 
-    # get a random sample of [batch_size] memories
     def sample(self, batch_size):
+        """get a random sample of [batch_size] memories"""
         return random.sample(self.buffer, batch_size)
 
     def __len__(self):
